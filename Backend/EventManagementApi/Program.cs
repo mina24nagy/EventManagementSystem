0fcf0372cb7785421b471b529f011a30b52bade2
mina24nagy <mina24nagy@gmail.com>
2024-11-23 19:07:58 +0200

    Add project files.

---
+++ Backend/EventManagementApi/Program.cs
@@ -0,0 +1,55 @@
+using EventManagementBusiness;
+using EventManagementBusiness.IServices;
+using EventManagementBusiness.Services;
+using EventManagementData;
+using EventManagementData.Models.IRepositories;
+using EventManagementData.Models.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace EventManagementApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors();
+
+            builder.Services.AddScoped<GlobalExceptionHandler>();
+            builder.Services.AddDbContext<EventDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddAutoMapper(typeof(EventMappingProfile).Assembly);
+            builder.Services.AddScoped<IEventRepository, EventRepository>();
+            builder.Services.AddScoped<IEventService, EventService>();
+
+            builder.Host.UseSerilog((context, configuration) =>
+                configuration.ReadFrom.Configuration(context.Configuration));
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseMiddleware<GlobalExceptionHandler>();
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
